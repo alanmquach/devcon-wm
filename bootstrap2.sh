b28f2e8f797d05dcf542fa46c2d9f4c641faa23f
alanmquach <integsrtite@gmail.com>
2017-12-29 04:23:46 -0800

    Adding xstartup for vnc to launch i3

--- bootstrap2.sh
+++ bootstrap2.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+NEWUSER=$1
+AUTHORIZEDKEYS=$2
+
 # bootstrap2
 echo "Configuring remote access (xrdp)"
 cat /root/xrdp.ini > /etc/xrdp/xrdp.ini
@@ -8,3 +11,9 @@ echo "Starting xrdp"
 /etc/xrdp/xrdp.sh start
 echo "Remote access (xrdp) enabled (vncserver still needs to be started manually)"
 
+if [ -f "/home/$NEWUSER/.vnc/xstartup" ]; then
+    echo "/home/$NEWUSER/.vnc/xstartup, leaving it alone"
+else
+    echo "/home/$NEWUSER/.vnc/xstartup not found, it needs to just launch i3. Copying from template..."
+    su -c "mkdir ~/.vnc && cat /tmp/xstartup > ~/.vnc/xstartup && chmod a+x ~/.vnc/xstartup" $NEWUSER    
+fi
